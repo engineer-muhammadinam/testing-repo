f00731b4e8d245baf1f59893d53a4795deabbaf5
Muhammad Inam <muhammadinam247@gmail.com>
2026-09-10 12:33:43 +0500

    add substraction code

--- math.py
+++ math.py
@@ -2,3 +2,6 @@ first_number = input("Enter the first number: ")
 second_number = input("Enter the second number: ")
 sum_result = float(first_number) + float(second_number)
 print("The sum of", first_number, "and", second_number, "is:", sum_result)
+
+subtraction_result = float(first_number) - float(second_number)
+print("The difference between", first_number, "and", second_number, "is:", subtraction_result)
